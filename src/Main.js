ef065e2203182d049e52afd8b920bd9e6a89a530
Ruhan Asif <theruhaanasif@gmail.com>
2021-09-18 20:43:33 +0530

    Make the app stateful; i.e containers

--- src/Main.js
+++ src/Main.js
@@ -1,33 +1,40 @@
-import Scroll from "./Scroll.js"
-
+import Scroll from "./Scroll.js";
+import { useState } from "react";
+import React from 'react';
 
 function Main(){
+	const [yourName, setYourname] = useState("lovely");
+	var name;
+	function storeName(event) {
+		name = event.target.value;
+		event.preventDefault();	
+	};
+	function setName(event) {
+		setYourname(name);
+	};
+	
 	return(
 		<div className="container">
-			
 			<div className="form">
-				
-				<input type="text" placeholder="creepy pharma" /> 
-				<button className="button-initials">Register Your Initials</button> <br />
-				<label>What bothers you lovely</label> <br />
+				<input type="text" onChange={storeName} placeholder="creepy pharma"/> 
+				<button onClick={setName} className="button-initials">Register Your Initials</button> <br />
+				<label>What bothers you lovely {yourName}</label> <br />
 				<textarea className="big-textfield" rows="23" cols="30" placeholder="let it all out, pharma cares for you" /> <br />
 				<button className="purple-button" type="submit">Don't be scared my love</button>
 			</div>
 			<div className="recents">
 				<p>They makes us feel as if we don't matter, but you do really matter :)</p>
-				
 				<Scroll>
-				
-				<p>
-				 leo eget bibendum. Morbi varius tincidunt magna, eu eleifend turpis lobortis quis. Vivamus nibh risus, gravida quis nulla sed, vulputate finibus purus. Suspendisse potenti. Sed efficitur risus mattis pulvinar malesuada.
-				</p>
-
+			  	  <p>
+			 
+				  </p>
 				</Scroll>
 				
 
 			</div>
 			
 		</div>
+		
 	);
 }
 
